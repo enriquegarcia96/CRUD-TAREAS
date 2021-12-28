b119bb864c395bc9231a4f7a0c3cbf6d95b0e99b
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-12-27 21:29:53 -0600

    feat: 🎸 eliminar y completar la tarea selecionada

--- src/app/tareas/listado-tareas/listado-tareas.component.ts
+++ src/app/tareas/listado-tareas/listado-tareas.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Tarea } from '../interfaces/tarea';
+import { TareasService } from '../services/tareas.service';
 
 @Component({
   selector: 'app-listado-tareas',
   templateUrl: './listado-tareas.component.html',
   styleUrls: ['./listado-tareas.component.css'],
 })
-export class ListadoTareasComponent {
-  tareas: Array<string> = ['Barrer', 'Trapear', 'Cocinar', 'Leer'];
+export class ListadoTareasComponent  {
+
+  constructor(private tareasService: TareasService) {}
+
+  get tareas(){
+    return this.tareasService.obtenerTareas();
+  }
+
+  delete(tarea: Tarea){
+    this.tareasService.deleteTarea(tarea.tarea);
+  }
+
+  complete(tarea: Tarea){
+    this.tareasService.completeTarea(tarea.tarea);
+  }
+
+
+
 }
