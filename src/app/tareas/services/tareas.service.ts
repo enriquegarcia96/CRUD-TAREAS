69b0fcbe17e54a84a0119e35a77360a65f3ea874
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-12-27 21:31:28 -0600

    refactor: 💡 cambiando a localstorage como base de datos

--- src/app/tareas/services/tareas.service.ts
+++ src/app/tareas/services/tareas.service.ts
@@ -1,47 +1,43 @@
 import { Injectable } from '@angular/core';
 import { Tarea } from '../interfaces/tarea';
 
-
 @Injectable()
 export class TareasService {
   tareas: Array<Tarea> = [];
 
-  deleteTarea(nombreTarea: string){
+  deleteTarea(nombreTarea: string) {
     const tareas = this.obtenerTareas();
 
     tareas.forEach((tarea, index) => {
-      if(tarea.tarea === nombreTarea){
-        tareas.splice(index, 1)
+      if (tarea.tarea === nombreTarea) {
+        tareas.splice(index, 1);
       }
-    })
+    });
     localStorage.setItem('tareas', JSON.stringify(tareas));
   }
 
-  obtenerTareas(){
-
+  obtenerTareas() {
     if (localStorage.getItem('tareas') === null) {
       this.tareas = [];
     } else {
-      this.tareas = JSON.parse(localStorage.getItem('tareas')!)
+      this.tareas = JSON.parse(localStorage.getItem('tareas')!);
     }
     return this.tareas;
   }
 
-
-  completeTarea(nombreTarea: string){
+  completeTarea(nombreTarea: string) {
     const tareas = this.obtenerTareas();
 
-    tareas.find((tarea) =>{
+    tareas.find((tarea) => {
       if (tarea.tarea === nombreTarea) {
-        tarea.completada = true
+        tarea.completada = true;
       }
-    })
+    });
     localStorage.setItem('tareas', JSON.stringify(tareas));
   }
 
-  agregarTarea(tarea: Tarea){
+  agregarTarea(tarea: Tarea) {
     this.tareas.push(tarea);
     localStorage.setItem('tareas', JSON.stringify(this.tareas));
   }
-
 }
