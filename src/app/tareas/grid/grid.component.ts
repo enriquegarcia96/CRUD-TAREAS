eb1cbc3acb9e4d476656306d8eee09fe5e9b6658
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-12-27 21:34:43 -0600

    feat: 🎸 kjada

--- src/app/tareas/grid/grid.component.ts
+++ src/app/tareas/grid/grid.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grid',
-  templateUrl: './grid.component.html'
+  templateUrl: './grid.component.html',
+  styleUrls: ['./grid.component.css']
 })
 export class GridComponent   {
 
